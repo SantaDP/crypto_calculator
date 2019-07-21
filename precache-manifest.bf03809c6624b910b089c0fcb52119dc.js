1a9ac5e3f9ec37e14ef1636600aed99af7ee667d
Aleksandr Pereleev <aleksandr.pereleev@gmail.com>
2019-07-21 17:05:48 +0300

    Updates

---
+++ precache-manifest.bf03809c6624b910b089c0fcb52119dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e1f2c5d26ce0f919d3c53973b02858",
+    "url": "/crypto_calculator/index.html"
+  },
+  {
+    "revision": "bb8ae5904f5713d9af33",
+    "url": "/crypto_calculator/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/crypto_calculator/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "bb8ae5904f5713d9af33",
+    "url": "/crypto_calculator/static/js/main.a9d7a70a.chunk.js"
+  },
+  {
+    "revision": "f260c62a6013c18fd9a7",
+    "url": "/crypto_calculator/static/js/runtime~main.3280b272.js"
+  }
+]);
